720e0198aa7b65aa89821b27a42f6411eb841271
HeatherDa <crystal_tree@hotmail.com>
2016-05-04 15:10:11 -0500

    Gui version 1

--- src/com/Heather/Main.java
+++ src/com/Heather/Main.java
@@ -5,10 +5,10 @@ import java.util.*;
 
 public class Main {
     static final String JDBC_DRIVER = "jdbc:mysql://localhost:3306/";
-    static final String DB_CONNECTION_URL = "cube";
+    static final String DB_CONNECTION_URL = "music";
 
-    static final String USER = "root";
-    static final String PASSWORD = "itecitec";
+    static final String USER = "Heather";
+    static final String PASSWORD = "ashlynn8";
 
     public static void main(String[] args) throws SQLException {
 	// write your code here
@@ -21,7 +21,9 @@ public class Main {
         Connection connect=DriverManager.getConnection(DB_CONNECTION_URL, USER, PASSWORD);
         Statement state=connect.createStatement();
 
-        state.execute("CREATE TABLE if NOT EXISTS Songs (SongID PRIMARY, Title VARCHAR (100), Composer VARCHAR (50), Genre VARCHAR (50), Style VARCHAR (50), Lyrics BOOLEAN, BookID INT, AlbumID int)");
-        state.execute("CREATE TABLE if NOT EXISTS Print (BookID PRIMARY, TimeSignature VARCHAR (5), KeySignature VARCHAR (5), FirstPage VARCHAR (4), TotalPages DOUBLE, Location VARCHAR ())");
+        state.execute("CREATE TABLE if NOT EXISTS Songs (SongID PRIMARY, Title VARCHAR (100), Composer VARCHAR (50), Genre VARCHAR (50), Style VARCHAR (50), Lyrics BOOLEAN, Format VARCHAR (10), ContainerID int)"); //ContainerID is either a BookID or an AlbumID, or "none" for loose papers
+        state.execute("CREATE TABLE if NOT EXISTS Papers (PapersID PRIMARY, Box BOOLEAN, Location VARCHAR (15))");
+        state.execute("CREATE TABLE if NOT EXISTS Book (BookID PRIMARY, Name VARCHAR (100), TimeSignature VARCHAR (5), KeySignature VARCHAR (5), FirstPage VARCHAR (4), TotalPages DOUBLE, Location VARCHAR (15))");//books, pamphlets, loose sheets
+        state.execute("CREATE TABLE if NOT EXISTS Audio (AlbumID PRIMARY, Album VARCHAR (100), Artist VARCHAR (50), Location VARCHAR (15))");
     }
 }
---
+++ src/com/Heather/MusicGui.java
@@ -0,0 +1,34 @@
+package com.Heather;
+
+import javax.swing.*;
+
+/**
+ * Created by cryst on 5/4/2016.
+ */
+public class MusicGui {
+    private JPanel rootPanel;
+    private JTextField songTitleTextBox;
+    private JTextField composerTextField;
+    private JComboBox formatComboBox;
+    private JComboBox styleComboBox;
+    private JComboBox genreComboBox;
+    private JRadioButton lyricsRadioButton;
+    private JTable albumBookTitlesTable;
+    private JButton addNewAlbumButton;
+    private JButton addNewBookButton;
+    private JButton searchButton;
+    private JComboBox keyComboBox;
+    private JTextField timeSignatureTextField;
+    private JTextField firstPageTextField;
+    private JTextField durationTextField;
+    private JButton addSongButton;
+    private JTextField albumTitleTextField;
+    private JTextField artistOrGroupTextField;
+    private JComboBox albumLocationComboBox;
+    private JTextField bookTitleTextField;
+    private JTextField ISBNTextField;
+    private JComboBox bookLocationComboBox;
+    private JComboBox searchSongTitleComboBox;
+    private JComboBox searchBookTitleComboBox;
+    private JComboBox searchAlbumTitleComboBox;
+}
